6e28659088f4ff8db05d97802e01c4db6a871363
Julian Horner <julianhorner@web.de>
2019-12-16 18:40:52 +0100

    Add eureka client to dummy-service

--- src/main/java/de/rtuni/ms/ds/Application.java
+++ src/main/java/de/rtuni/ms/ds/Application.java
@@ -5,19 +5,18 @@
 
 package de.rtuni.ms.ds;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
+ * Dummy service.
+ * 
  * @author Julian
  *
  */
 @SpringBootApplication
+@EnableEurekaClient     
 public class Application {
     //---------------------------------------------------------------------------------------------
     
@@ -26,26 +25,7 @@ public class Application {
      * 
      * @param args The arguments
      */
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static void main(final String[] args) { SpringApplication.run(Application.class, args); }
 
-    //---------------------------------------------------------------------------------------------
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-    
     //---------------------------------------------------------------------------------------------
 }
