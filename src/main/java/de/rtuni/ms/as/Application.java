970e19a0746494b999d14048dcc0b6b522b96fb0
Julian Horner <julianhorner@web.de>
2019-12-17 15:48:14 +0100

    Add basic application class and configuration to authentication service

---
+++ src/main/java/de/rtuni/ms/as/Application.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 (C) by Julian Horner.
+ * All Rights Reserved.
+ */
+
+package de.rtuni.ms.as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Authentication service for microservice architecture.
+ * 
+ * @author Julian
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient     
+public class Application {
+    //---------------------------------------------------------------------------------------------
+    
+    /**
+     * Starts the application.
+     * 
+     * @param args The arguments
+     */
+    public static void main(final String[] args) { SpringApplication.run(Application.class, args); }
+
+    //---------------------------------------------------------------------------------------------
+}
